069e9ef96f9fc8debb3bb5f6c1b76c1e8f08d6f0
Diana Liu <dianaliu@Dianas-MacBook-Air.local>
2024-10-06 10:53:15 +1000

    Delete build files

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,8 +6,18 @@ internal class Program{
     static void Main(string[] args){
         Class1 lib1 = new Class1();
         string message = lib1.GetMessage("Diana");
-
         Console.WriteLine(message);
+
+        // type transform explicit
+        string str1 = "123";
+        int result;
+        bool success = int.TryParse(str1, out result);
+        Console.WriteLine(success);
+
+        string str2 = "abx";
+        int result2;
+        bool success2 = int.TryParse(str2, out result2);
+        Console.WriteLine(success2);
     }
 
 }
